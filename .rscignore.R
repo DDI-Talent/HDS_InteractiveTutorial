ca0e3e671202a7e105f410446fe679fcd4b8b007
Daniel <dvicencio947@gmail.com>
2024-11-25 13:24:08 +0000

    rscignore file didn't work; trying now using directories and files only.

--- .rscignore.R
+++ .rscignore.R
@@ -1,5 +1,4 @@
-Archive/HDS_SettingUpR.html 
-Archive/HDS_SettingUpR.Rmd 
-Archive/Interactive_Coding_Exercise.html
-Archive/Interactive_Coding_Exercise.Rmd
-R-Basics.html 
+Archive
+.github
+.Rprofile
+R-Basics.html
